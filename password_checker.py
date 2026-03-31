38dc9c2069f3bd22f1cba5fd2925fb8c007b12eb
sameershaik59763-hue <sameershaik59763@gmail.com>
2026-04-01 01:48:34 +0530

    Implement password strength checker function

---
+++ password_checker.py
@@ -0,0 +1,27 @@
+import re
+
+def check_password_strength(password):
+    score = 0
+
+    if len(password) >= 8:
+        score += 1
+    if re.search("[a-z]", password):
+        score += 1
+    if re.search("[A-Z]", password):
+        score += 1
+    if re.search("[0-9]", password):
+        score += 1
+    if re.search("[@#$%^&*]", password):
+        score += 1
+
+    if score <= 2:
+        return "Weak Password ❌"
+    elif score <= 4:
+        return "Medium Password ⚠️"
+    else:
+        return "Strong Password ✅"
+
+password = "Test@123"
+
+print("Password:", password)
+print(check_password_strength(password))
